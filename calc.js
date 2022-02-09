fc8ae838c6a88dba19bf1eac4c42a94a5beb38a1
Anaglebia <bia22.sp@icloud.com>
2022-02-08 23:39:17 -0300

    Criação da calculadora

---
+++ calc.js
@@ -0,0 +1,19 @@
+function divisao(a,b){
+    return a/b
+}
+
+function multiplicacao(a,b){
+    return a*b
+}
+
+function subtracao(a,b){
+    return a*b
+}
+
+function soma(a,b){
+    return a*b
+}
+
+
+
+module.exports={divisao, multiplicacao, subtracao, soma}
---
+++ index.js
@@ -0,0 +1,8 @@
+const calc=require('./calc')
+
+console.log(calc.divisao(4,2))
+console.log(calc.multiplicacao(4,2))
+console.log(calc.subtracao(4,2))
+console.log(calc.soma(4,2))
+
+
